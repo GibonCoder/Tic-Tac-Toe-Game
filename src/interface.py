403052cc2ebdfb6ebf792988dc994fdd7a43e504
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-02-12 22:56:06 +0100

    Create 'decision' method

--- src/interface.py
+++ src/interface.py
@@ -2,6 +2,17 @@ class Interface:
     def __init__(self):
         self._player_char = ''
         self._bot_char = ''
+        self.continue_game = True
 
+    def choose_char(self, char):
+        match char.upper():
+            case 'X':
+                self._player_char = 'X'
+                self._bot_char = 'O'
+            case 'O':
+                self._player_char = 'O'
+                self._bot_char = 'X'
+            case '0':
+                self.continue_game = False
 
 
