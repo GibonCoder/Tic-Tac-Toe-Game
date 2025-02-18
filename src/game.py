5c194428d92bdddb46c9e0b54bbc06e35b1e4325
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-02-18 23:31:50 +0100

    Write function for validating moves

--- src/game.py
+++ src/game.py
@@ -1,5 +1,6 @@
 from transform_column import transform_column
 
+
 class Game:
     def __init__(self):
         self.board = [[' ' for _ in range(3)] for _ in range(3)]
@@ -14,3 +15,9 @@ class Game:
         transformed_col = transform_column(col)
         self.board[row-1][transformed_col] = player
 
+    def validate_move(self, row, col):
+        if row < 1 or row > 3:
+            return False
+        elif self.board[row-1][col] != ' ':
+            return False
+        return True
