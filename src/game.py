070831e7d16c345dbcf8e79236e1260bb55f2196
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-02-18 22:47:57 +0100

    Create Game class. Write function for board printing

---
+++ src/game.py
@@ -0,0 +1,8 @@
+class Game:
+    def __init__(self):
+        self.board = [[' ' for _ in range(3)] for _ in range(3)]
+
+    def print_board(self):
+        for row in self.board:
+            print(('|'.join(row)))
+            print('_'*5)
