89449c8a8aabd22096df54a02fb2ba387b3884c1
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-02-20 10:26:44 +0100

    Create HumanPlayer that inheritate from Player

--- src/players.py
+++ src/players.py
@@ -5,3 +5,9 @@ class Player(ABC):
     @abstractmethod
     def get_move(self):
         pass
+
+
+class HumanPlayer(Player):
+    def get_move(self):
+        cell = input("Enter cell where you want to place your move (e.g. A1): ")
+        return cell
