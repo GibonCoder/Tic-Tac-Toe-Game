e754669ad1aff049ef4941a8ee15129f225bbc76
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-02-20 16:19:20 +0100

    Create setter and getter methods

--- src/players.py
+++ src/players.py
@@ -2,12 +2,33 @@ from abc import ABC, abstractmethod
 
 
 class Player(ABC):
+    @property
+    @abstractmethod
+    def character(self):
+        pass
+
+    @character.setter
+    @abstractmethod
+    def character(self, char):
+        pass
+
     @abstractmethod
     def get_move(self):
         pass
 
 
 class HumanPlayer(Player):
+    def __init__(self):
+        self._character = None
+
+    @property
+    def character(self):
+        return self._character
+
+    @character.setter
+    def character(self, char):
+        self._character = char
+
     def get_move(self):
         cell = input("Enter cell where you want to place your move (e.g. A1): ")
         return cell
