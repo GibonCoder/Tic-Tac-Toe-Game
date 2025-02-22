213413a74e64d2a893b38f0ff8fd5a21e2786d1d
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-02-22 22:17:28 +0100

    Write logic for checking if it a win or a tie.

--- src/main.py
+++ src/main.py
@@ -15,3 +15,9 @@ for _ in range(5):
     move = computer.get_move()
     game.place_move(move, computer.character)
     game.print_board()
+    if game.check_win():
+        print('Game over!')
+        break
+    elif game.is_tie():
+        print('It\'s a tie!')
+        break
