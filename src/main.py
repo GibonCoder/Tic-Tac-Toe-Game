3fb8814fb0a765c88ee750b26a2cf6a3eb916687
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-02-22 22:09:56 +0100

    Initialize, Game, HumanPlayer & ComputerPlayer objects

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from game import Game
+from players import HumanPlayer, ComputerPlayer
+
+game = Game()
+human = HumanPlayer()
+human.character('X')
+computer = ComputerPlayer()
+computer.character('O')
+
+
+
