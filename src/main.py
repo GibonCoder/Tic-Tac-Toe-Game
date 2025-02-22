ae45b18b42dc949b081b586246d3638fad6f72bf
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-02-22 23:21:08 +0100

    Add validation loop for player input. BUG REPORT: Despite validation, player can overwrite computers move

--- src/main.py
+++ src/main.py
@@ -10,11 +10,15 @@ computer.character = 'O'
 for _ in range(5):
     game.print_board()
     move = human.get_move()
+    while game.validate_move(move):
+        move = human.get_move()
     game.place_move(move, human.character)
     game.print_board()
     move = computer.get_move(game.board)
+    # TODO: FIX 1
+    while game.validate_move(move):
+        move = computer.get_move(game.board)
     game.place_move(move, computer.character)
-    game.print_board()
     if game.check_win():
         print('Game over!')
         break
